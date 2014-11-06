da7103b0258b08edda62d4712b0617d38ca42fa6
Rafael Birkmann <rafkafu@yahoo.de>
2014-11-06 17:17:19 +0100

    responsive navigation lvl2 toggle

--- source/js/modules/navigation.js
+++ source/js/modules/navigation.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+
 	$(".menu-toggle").click(function(){
-		$(".nav-main ul").toggle();
+		$(".nav-main > ul").slideToggle();
 	});
+
+	// responsive subnav
+	$(".nav-main li:has(ul)").addClass("menu-with-sub");
+	$('.menu-with-sub').prepend("<div class=\"sub-toggle\">+</div>");
+
+	$(".sub-toggle").click(function(){
+		$(this).parent().find("ul").slideToggle();;
+		$(this).parent().toggleClass("open");
+	});
+
 });
